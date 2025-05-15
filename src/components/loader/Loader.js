edc8dbfb13fc0e8a815f09a53b4bb9da53a4c664
daniel rozenblat <danielroz12345@gmail.com>
2025-05-15 09:18:21 +0300

    newChange

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -4,13 +4,14 @@ import styles from './Loader.module.css';
 const LoadingEffect = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.heartContainer}>
-        <div className={styles.heart}></div>
-        <div className={styles.sparkle}></div>
-        <div className={styles.sparkle} style={{ animationDelay: '0.4s' }}></div>
-        <div className={styles.sparkle} style={{ animationDelay: '0.8s' }}></div>
-      </div>
-      <span className={styles.text}>טוען...</span>
+    <div className={styles['typing-indicator']}>
+      <div className={styles['typing-circle']}></div>
+      <div className={styles['typing-circle']}></div>
+      <div className={styles['typing-circle']}></div>
+      <div className={styles['typing-shadow']}></div>
+      <div className={styles['typing-shadow']}></div>
+      <div className={styles['typing-shadow']}></div>
+    </div>
     </div>
   );
 };
